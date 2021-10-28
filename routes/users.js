e5c64eb0b52d067101ad48c16550fb9d430d3c2f
jawahar-reddy <62792283+jawahar-reddy@users.noreply.github.com>
2021-10-27 23:37:09 -0500

    updated users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var a;
+var log2;
+var cosh;
+var floor;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get("/", function (req, res, next) {
+  a = Math.random() * (100 - 1) + 1;
+  a1 = Math.random() * (100 - 1) + 1;
+
+  log2 = Math.log2(a);
+
+  cosh = Math.cosh(a);
+
+  floor = Math.floor(a,a1);
+
+  res.send(`(log2) applied to (${a}) is ${log2} <br>
+
+            (cosh) applied to (${a}) is ${cosh} <br>
+
+            (floor) applied to (${a},${a1}) is ${floor}`);
 });
 
 module.exports = router;
